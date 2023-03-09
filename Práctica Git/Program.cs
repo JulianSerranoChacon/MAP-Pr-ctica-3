653de71fd25d4abd3d5114f7cca9ed1c70b04b75
VerXxuxZ <juserr04@ucm.es>
2023-03-09 12:31:59 +0100

    primer commit

---
+++ Práctica Git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Práctica_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
