6c72f0140c73b52fc9fc6b9d82a2de086ec55e47
JulianSerranoChacon <juserr04@ucm.es>
2023-03-16 12:42:36 +0100

    Cambios para conflicto tipo 1

--- Práctica Git/Program.cs
+++ Práctica Git/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("APUPIPIAPOOPAISAPIPAOSPAPOSOAPIPIIPOIPAP");
+            Console.WriteLine("APUPIPIvkxjvlijxñzAPOOPAISAPIPAOSPAPOSOAPIPIIPOIPAP");
         }
     }
 }
