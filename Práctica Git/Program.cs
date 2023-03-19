488f4645ed33614d093c84475159d622db35b925
JulianSerranoChacon <juserr04@ucm.es>
2023-03-19 20:15:56 +0100

    Queremos volvera a versión anterior

--- Práctica Git/Program.cs
+++ Práctica Git/Program.cs
@@ -8,7 +8,7 @@
             Console.WriteLine("APUPIPIvkxjvlijxñzAPOOPAISAPIPAOSPAPOSOAPIPIIPOIPAP");
             Console.WriteLine("Viva España");
             float patata;
-            Esto claramente debe de desaparecerbaba;
+            //Esto claramente debe de desaparecerbabafFWXFA;
         }
     }
 }
